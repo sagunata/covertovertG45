9706233dfe3b836e081b0f4472febfbb8f0ca429
sagunata <e252178@metu.edu.tr>
2024-10-25 16:42:15 +0300

    Update icmp_sender.py

--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,10 @@
 import scapy
 
-# Implement your ICMP sender here
+def send_ICMP_request(target_ip):
+  packet = IP(dst=target, ttl=1) / ICMP()
+
+  send(packet)
+
+if __name__ == "__main__":
+    target = "172.18.0.3"
+    send_ICMP_request(target)
