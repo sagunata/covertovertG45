9709082470509c18680d34db23d84f761dd233b5
sagunata <e252178@metu.edu.tr>
2024-10-25 16:47:04 +0300

    Update icmp_sender.py

--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -4,6 +4,7 @@ def send_ICMP_request(target_ip):
   packet = IP(dst=target, ttl=1) / ICMP()
 
   send(packet)
+  print(f"ICMP request sent to {target} with TTL=1")
 
 if __name__ == "__main__":
     target = "172.18.0.3"
