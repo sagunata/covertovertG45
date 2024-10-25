b9b92348a47654156d24ad1c5325c2b9b0ab8396
sagunata <e252178@metu.edu.tr>
2024-10-25 16:45:49 +0300

    Update icmp_receiver.py

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,13 @@
 import scapy
 
-# Implement your ICMP receiver here
+def pHandler(packet):
+  if packet.haslayer(ICMP) and packet[ICMP].type == 8:
+    print("Received ICMP request packet:")
+    packet.show()
+
+def start_sniffing():
+  print("Listening for ICMP packets...")
+  sniff(filter="icmp", prn=pHandler, store=0)
+
+if __name__ == "__main__":
+    start_sniffing()
